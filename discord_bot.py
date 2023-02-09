3479f5cf35ad366da1694725641fa23de5e7e86e
Joshua Maldonado <70040511+Ub3rJosh@users.noreply.github.com>
2023-02-09 07:08:34 -0500

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -4,6 +4,8 @@ Created on Tue Nov 22 08:28:49 2022
 
 @author: Joshua Maldonado
 @email:  joshuamaldonado4432@gmail.com
+
+this is a test to see if a change will trigger the bot scraper
 """
 
 #from useful_functions import *
